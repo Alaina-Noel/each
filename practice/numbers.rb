d2deea1503d527da8b6d9e9fc4501d1fd0dd2a89
Meg Stang <mstang15@gmail.com>
2022-02-01 12:13:42 -0700

    Add possible solutions for the practice files

--- practice/numbers.rb
+++ practice/numbers.rb
@@ -3,7 +3,69 @@
 numbers = [1,2,3,4,5]
 
 # 1. Can you create a new array with only the odd numbers?
+
+            # odd_nums = []
+
+            # numbers.each do |num|
+            #     if num.odd?
+            #         odd_nums << num
+            #     end 
+            # end 
+
+            # p odd_nums 
+
+
 # 2. Can you create a new array with only the even numbers?
+
+            # even_nums = []
+
+            # numbers.each do |num|
+            #     if num.odd?
+            #         even_nums << num
+            #     end 
+            # end 
+
+            # p even_nums 
+
+
+
 # 3. Can you print out each number doubled?
+
+            # doubled = []
+
+            # numbers.each do |num|
+            #     p num * 2
+            # end 
+
+
 # 4. Can you print out if the number is divisible by 2 or not?
+
+            # numbers.each do |num|
+            #     if num % 2 == 0
+            #         p true
+            #     else 
+            #         p false
+            #     end 
+            # end 
+
+# OR 
+
+            # numbers.each do |num|
+            #     if num.odd? 
+            #         p false 
+            #     else 
+            #         p true 
+            #     end 
+            # end 
+
+
 # 5. Can you find the the sum of the numbers?
+
+
+        # sum = 0
+
+        # numbers.each do |num|
+        #     sum += num
+        # end 
+
+        # p sum 
--- practice/singers.rb
+++ practice/singers.rb
@@ -3,7 +3,50 @@
 singers = ["billie", "ariana", "lizzo", "leon"]
 
 # 1. Can you print out their names capitalized?
+
+        # singers.each do |singer|
+        #     p singer.capitalize
+        # end 
+
+
 # 2. Can you print out their names in all caps?
+
+
+        # singers.each do |singer|
+        #     p singer.upcase
+        # end 
+
+
 # 3. Can you print out their names but reversed? (["leon", "lizzo", "ariana", "billie"])
+
+        # reversed_names = []
+
+        # singers.each do |singer|
+        #     reversed_names.unshift(singer)
+        # end 
+
+        # p reversed_names
+
+
 # 4. Can you create a new array with only the names that are longer than four letters in length?
+
+        # names_longer_than_four_letters = []
+
+        # singers.each do |singer|
+        #     if singer.length > 4
+        #         names_longer_than_four_letters << singer
+        #     end 
+        # end 
+
+        # p names_longer_than_four_letters
+
+
 # 5. Can you create a new array with the lengths of their names?
+
+        # lengths_of_names = []
+
+        # singers.each do |singer|
+        #    lengths_of_names << singer.length
+        # end 
+
+        # p lengths_of_names
